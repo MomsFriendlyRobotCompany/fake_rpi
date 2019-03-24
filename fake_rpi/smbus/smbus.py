30c526744f5f16694ef447e48a6c0ff3f0a4ff1a
Wiktor <wiktor@pykonik.org>
2019-03-24 22:05:34 +0100

    Generated values shouldn't be greater than 255
    Fixes #10

--- fake_rpi/smbus/smbus.py
+++ fake_rpi/smbus/smbus.py
@@ -17,18 +17,18 @@ class SMBus(Base):
 	@printf
 	def write_byte(self, i2c_addr, value):
 		pass
-	
+
 	@printf
 	def read_byte_data(self, i2c_addr, register):
-		return randint(0, 2**8)
+		return randint(0, 2**8-1)
 
 	@printf
 	def read_byte(self, i2c_addr):
-		return randint(0, 2**8)
+		return randint(0, 2**8-1)
 
 	@printf
 	def read_word_data(self, i2c_addr, register):
-		return [randint(0, 2**8)]*2
+		return [randint(0, 2**8-1)]*2
 
 	@printf
 	def write_word_data(self, i2c_addr, register, value):
@@ -36,7 +36,7 @@ class SMBus(Base):
 
 	@printf
 	def read_i2c_block_data(self, a, b, c):
-		return [randint(0, 2**8)]*c
+		return [randint(0, 2**8-1)]*c
 
 	@printf
 	def write_i2c_block_data(self, i2c_addr, register, data):
