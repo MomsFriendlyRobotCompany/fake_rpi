fbc6a1b9d42fff286db4caf543729c7e76d451c8
walchko <walchko@users.noreply.github.com>
2020-02-04 19:17:42 -0500

    up

--- fake_rpi/picamera.py
+++ fake_rpi/picamera.py
@@ -5,50 +5,50 @@ from .Base import Base
 
 
 class BGR(object):
-	"""Fake class"""
+    """Fake class"""
 
-	@printf
-	def __init__(self, sz):
-		# constructor
-		self.array = np.random.rand(*sz)
+    @printf
+    def __init__(self, sz):
+        # constructor
+        self.array = np.random.rand(*sz)
 
-	@printf
-	def truncate(self, num):
-		# refreshes the fake image
-		self.array = np.random.rand(*self.array.shape)
+    @printf
+    def truncate(self, num):
+        # refreshes the fake image
+        self.array = np.random.rand(*self.array.shape)
 
 
 # class picamera(object):
-# 	"""Fake class"""
+#     """Fake class"""
 class PiCamera(Base):
-	"""Fake class"""
-	resolution = (0, 0)
+    """Fake class"""
+    resolution = (0, 0)
 
-	def __init__(self):
-		# empty constructor
-		# print('WARNING: Fake_RPi PiCamera on {}'.format(platform.system().lower()))
-		Base.__init__(self, self.__class__)
-		pass
+    def __init__(self):
+        # empty constructor
+        # print('WARNING: Fake_RPi PiCamera on {}'.format(platform.system().lower()))
+        Base.__init__(self, self.__class__)
+        pass
 
-	def __enter__(self):
-		return self
+    def __enter__(self):
+        return self
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		pass
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
-	def close(self):
-		# this does nothing
-		pass
+    def close(self):
+        # this does nothing
+        pass
 
-	@printf
-	def capture(self, image, format, use_video_port):
-		# this does nothing
-		pass
+    @printf
+    def capture(self, image, format, use_video_port):
+        # this does nothing
+        pass
 
 
 class array(object):
-	"""Fake class"""
+    """Fake class"""
 
-	@staticmethod
-	def PiRGBArray(cam, size):
-		return BGR(size)
+    @staticmethod
+    def PiRGBArray(cam, size):
+        return BGR(size)
